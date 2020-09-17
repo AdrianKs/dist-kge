bea077bdaa2183634e01f75cfee422c92e39c4f6
AdrianKs <adrian.kochsiek@outlook.de>
2020-09-17 12:29:22 +0200

    add barrier after each partition

--- kge/job/train.py
+++ kge/job/train.py
@@ -779,6 +779,9 @@ class TrainingJob(TrainingOrEvaluationJob):
                 self.model.get_p_embedder().global_to_local_mapper[:] = -1
                 self.model.get_p_embedder().push_back()
             self.work_scheduler_client.work_done()
+            # todo: this barrier is only added for the new schedule in 2d block
+            #  should be moved to the scheduler
+            self.parameter_client.barrier()
 
             # run hooks (may modify trace)
             for f in self.post_epoch_hooks:
