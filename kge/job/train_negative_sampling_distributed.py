9a03b0ad8dfb7dbbd0d1556d2308103d3b1a7d68
AdrianKs <adrian.kochsiek@outlook.de>
2020-12-18 18:50:41 +0100

    reformat distributed negative sampling with black

--- kge/job/train_negative_sampling_distributed.py
+++ kge/job/train_negative_sampling_distributed.py
@@ -45,18 +45,21 @@ class BatchDataset(torch.utils.data.Dataset):
     def __len__(self):
         if self.samples is None:
             return 0
-        return math.ceil(len(self.samples)/self.batch_size)
+        return math.ceil(len(self.samples) / self.batch_size)
 
     def __getitem__(self, idx):
         """Gets a complete batch based on an idx"""
-        return self.samples[idx*self.batch_size:min((idx+1)*(self.batch_size), len(self.samples))].long()
+        return self.samples[
+            idx
+            * self.batch_size : min((idx + 1) * (self.batch_size), len(self.samples))
+        ].long()
 
     def set_samples(self, samples: torch.Tensor):
         if self.shuffle:
             samples = samples.numpy()
             np.random.shuffle(samples)
             self.samples = torch.from_numpy(samples)
-            #self.samples = samples[torch.randperm(len(samples))]
+            # self.samples = samples[torch.randperm(len(samples))]
         else:
             self.samples = samples
 
@@ -94,13 +97,13 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
             torch.arange(dataset.num_entities(), dtype=torch.int),
             torch.arange(dataset.num_relations(), dtype=torch.int)
             + dataset.num_entities(),
-            ]
+        ]
         if optimizer is None:
             optimizer = KgeOptimizer.create(
                 config,
                 model,
                 parameter_client=parameter_client,
-                lapse_indexes=lapse_indexes
+                lapse_indexes=lapse_indexes,
             )
         # barrier to wait for loading of pretrained embeddings
         self.parameter_client.barrier()
@@ -116,8 +119,12 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
         self.load_batch = self.config.get("job.distributed.load_batch")
         self.entity_localize = self.config.get("job.distributed.entity_localize")
         self.relation_localize = self.config.get("job.distributed.relation_localize")
-        self.entity_async_write_back = self.config.get("job.distributed.entity_async_write_back")
-        self.relation_async_write_back = self.config.get("job.distributed.relation_async_write_back")
+        self.entity_async_write_back = self.config.get(
+            "job.distributed.entity_async_write_back"
+        )
+        self.relation_async_write_back = self.config.get(
+            "job.distributed.relation_async_write_back"
+        )
         self.entity_sync_level = self.config.get("job.distributed.entity_sync_level")
         self.relation_sync_level = self.config.get(
             "job.distributed.relation_sync_level"
@@ -140,7 +147,9 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
         # initialize the parameter server
         #  each worker takes as many entities as it can fit, inits and pushes
         #  init work is distributed by the work scheduler
-        if not init_for_load_only and not self.config.get("lookup_embedder.pretrain.model_filename"):
+        if not init_for_load_only and not self.config.get(
+            "lookup_embedder.pretrain.model_filename"
+        ):
             # only the first worker initializes the relations
             if self.parameter_client.rank == self.min_rank:
                 self.model.get_p_embedder().push_all()
@@ -155,13 +164,19 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
                 )
                 self.model.get_s_embedder()._normalize_embeddings()
                 push_tensor = torch.cat(
-                    (self.model.get_s_embedder()._embeddings.weight.data[:len(init_entities)].cpu(),
-                     self.model.get_s_embedder().optimizer_values[:len(init_entities)].cpu()),
-                    dim=1
+                    (
+                        self.model.get_s_embedder()
+                        ._embeddings.weight.data[: len(init_entities)]
+                        .cpu(),
+                        self.model.get_s_embedder()
+                        .optimizer_values[: len(init_entities)]
+                        .cpu(),
+                    ),
+                    dim=1,
                 )
                 self.parameter_client.push(
                     init_entities + self.model.get_s_embedder().lapse_offset,
-                    push_tensor.cpu()
+                    push_tensor.cpu(),
                 )
         self.parameter_client.barrier()
 
@@ -170,13 +185,21 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
         super()._prepare()
 
         self.num_examples = self.dataset.split(self.train_split).size(0)
-        self.dataloader_dataset = BatchDataset(self.dataset.split(self.train_split), batch_size=self.batch_size, shuffle=True)
-        mp_context = torch.multiprocessing.get_context("fork") if self.config.get("train.num_workers") > 0 else None
+        self.dataloader_dataset = BatchDataset(
+            self.dataset.split(self.train_split),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+        mp_context = (
+            torch.multiprocessing.get_context("fork")
+            if self.config.get("train.num_workers") > 0
+            else None
+        )
         self.loader = torch.utils.data.DataLoader(
             self.dataloader_dataset,
             collate_fn=self._get_collate_fun(),
             shuffle=False,  # shuffle needs to be False, since it is handled in the dataset object
-            #batch_size=self.batch_size,  # batch size needs to be 1 since it is handled in the dataset object
+            # batch_size=self.batch_size,  # batch size needs to be 1 since it is handled in the dataset object
             num_workers=self.config.get("train.num_workers"),
             worker_init_fn=_generate_worker_init_fn(self.config),
             pin_memory=self.config.get("train.pin_memory"),
@@ -199,12 +222,23 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
             for slot in [S, P, O]:
                 negative_samples.append(self._sampler.sample(triples, slot))
             unique_time = -time.time()
-            unique_entities = torch.unique(torch.cat((triples[:, [S, O]].view(-1),
-                                                      negative_samples[S].unique_samples(remove_dropped=False),
-                                                      negative_samples[
-                                                          O].unique_samples(remove_dropped=False))))
-            unique_relations = torch.unique(torch.cat((
-                triples[:, [P]].view(-1), negative_samples[P].unique_samples(remove_dropped=False))))
+            unique_entities = torch.unique(
+                torch.cat(
+                    (
+                        triples[:, [S, O]].view(-1),
+                        negative_samples[S].unique_samples(remove_dropped=False),
+                        negative_samples[O].unique_samples(remove_dropped=False),
+                    )
+                )
+            )
+            unique_relations = torch.unique(
+                torch.cat(
+                    (
+                        triples[:, [P]].view(-1),
+                        negative_samples[P].unique_samples(remove_dropped=False),
+                    )
+                )
+            )
             unique_time += time.time()
 
             # map ids to local ids
@@ -213,12 +247,18 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
             else:
                 # entity_mapper = torch.full((self.dataset.num_entities(),), -1, dtype=torch.long)
                 entity_mapper = self.entity_mapper_tensors.popleft()
-                entity_mapper[unique_entities] = torch.arange(len(unique_entities), dtype=torch.long)
+                entity_mapper[unique_entities] = torch.arange(
+                    len(unique_entities), dtype=torch.long
+                )
             if self.relation_sync_level == "partition":
                 relation_mapper = self.model.get_p_embedder().global_to_local_mapper
             else:
-                relation_mapper = torch.full((self.dataset.num_relations(),), -1, dtype=torch.long)
-                relation_mapper[unique_relations] = torch.arange(len(unique_relations), dtype=torch.long)
+                relation_mapper = torch.full(
+                    (self.dataset.num_relations(),), -1, dtype=torch.long
+                )
+                relation_mapper[unique_relations] = torch.arange(
+                    len(unique_relations), dtype=torch.long
+                )
             triples[:, S] = entity_mapper[triples[:, S]]
             triples[:, P] = relation_mapper[triples[:, P]]
             triples[:, O] = entity_mapper[triples[:, O]]
@@ -229,7 +269,13 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
             # for debugging reset the entity mapper to -1
             # entity_mapper[:] = -1
             self.entity_mapper_tensors.append(entity_mapper)
-            return {"triples": triples, "negative_samples": negative_samples, "unique_entities": unique_entities, "unique_relations": unique_relations, "unique_time": unique_time}
+            return {
+                "triples": triples,
+                "negative_samples": negative_samples,
+                "unique_entities": unique_entities,
+                "unique_relations": unique_relations,
+                "unique_time": unique_time,
+            }
 
         return collate
 
@@ -254,32 +300,35 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
         # move triples and negatives to GPU. With some implementaiton effort, this may
         # be avoided.
         result.prepare_time -= time.time()
-        #result.cpu_gpu_time -= time.time()
+        # result.cpu_gpu_time -= time.time()
         batch["triples"] = batch["triples"].to(self.device)
         for ns in batch["negative_samples"]:
             ns.positive_triples = batch["triples"]
         batch["negative_samples"] = [
             ns.to(self.device) for ns in batch["negative_samples"]
         ]
-        #result.cpu_gpu_time += time.time()
+        # result.cpu_gpu_time += time.time()
         result.unique_time += batch["unique_time"]
         if self.config.get("job.distributed.load_batch"):
             if self.entity_sync_level == "batch":
-                #result.unique_time -= time.time()
+                # result.unique_time -= time.time()
                 unique_entities = batch["unique_entities"]
                 # unique_entities = torch.unique(torch.cat((batch["triples"][:, [S,O]].view(-1), batch["negative_samples"][S].unique_samples(), batch["negative_samples"][O].unique_samples())))
-                #result.unique_time += time.time()
+                # result.unique_time += time.time()
 
                 result.ps_wait_time -= time.time()
                 if not self.entity_async_write_back:
                     for wait_value in self.optimizer.entity_async_wait_values:
-                          self.parameter_client.wait(wait_value)
+                        self.parameter_client.wait(wait_value)
                     self.optimizer.entity_async_wait_values.clear()
                 result.ps_wait_time += time.time()
                 if self.entity_localize:
                     self.model.get_s_embedder().localize(unique_entities)
                 result.pull_and_map_time -= time.time()
-                entity_pull_time, cpu_gpu_time = self.model.get_s_embedder()._pull_embeddings(unique_entities)
+                (
+                    entity_pull_time,
+                    cpu_gpu_time,
+                ) = self.model.get_s_embedder()._pull_embeddings(unique_entities)
                 result.pull_and_map_time += time.time()
                 result.entity_pull_time += entity_pull_time
                 result.cpu_gpu_time += cpu_gpu_time
@@ -297,7 +346,10 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
                 if self.relation_localize:
                     self.model.get_p_embedder().localize(unique_relations)
                 result.pull_and_map_time -= time.time()
-                relation_pull_time, cpu_gpu_time = self.model.get_p_embedder()._pull_embeddings(unique_relations)
+                (
+                    relation_pull_time,
+                    cpu_gpu_time,
+                ) = self.model.get_p_embedder()._pull_embeddings(unique_relations)
                 result.pull_and_map_time += time.time()
                 result.relation_pull_time += relation_pull_time
                 result.cpu_gpu_time += cpu_gpu_time
@@ -305,4 +357,3 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
         batch["labels"] = [None] * 3  # reuse label tensors b/w subbatches
         result.size = len(batch["triples"])
         result.prepare_time += time.time()
-
