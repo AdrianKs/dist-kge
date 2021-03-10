9c5239567c74c6b06eb9ad05113a4b644fa561fe
AdrianKs <adrian.kochsiek@outlook.de>
2021-03-10 09:52:55 +0100

    fix prepare-time measurement

--- kge/job/train_negative_sampling_distributed.py
+++ kge/job/train_negative_sampling_distributed.py
@@ -594,6 +594,8 @@ class TrainingJobNegativeSamplingDistributed(TrainingJobNegativeSampling):
                     pre_pull_time += time.time()
                     prepare_time += time.time()
                 except StopIteration:
+                    pre_pull_time += time.time()
+                    prepare_time += time.time()
                     if len(pre_load_batches) == 0:
                         epoch_done = True
 
